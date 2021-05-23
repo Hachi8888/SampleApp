239ab0701267023a6de3e12ed643df0acc856bd3
hachi8888 <53389277+Hachi8888@users.noreply.github.com>
2021-05-23 23:43:04 +0900

    [add] modal

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,13 +1,30 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { memo, useEffect, VFC } from "react";
-import { Center, Spinner, Wrap, WrapItem } from "@chakra-ui/react";
+import { memo, useCallback, useEffect, VFC } from "react";
+import {
+  Center,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  useDisclosure,
+  Spinner,
+  Wrap,
+  WrapItem,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  Stack,
+  FormControl,
+  Input
+} from "@chakra-ui/react";
 import { UserCard } from "../organisms/user/UserCard";
 import { useAllUsers } from "../../hooks/useAllUsers";
 
 export const UserManagement: VFC = memo(() => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const { getUsers, users, loading } = useAllUsers();
 
   useEffect(() => getUsers(), []);
+  const onClickUser = useCallback(() => onOpen(), []);
 
   return (
     <>
@@ -23,11 +40,26 @@ export const UserManagement: VFC = memo(() => {
                 imageUrl="https://source.unsplash.com/random"
                 userName={user.username}
                 fullName={user.name}
+                onClickUser={onClickUser}
               />
             </WrapItem>
           ))}
         </Wrap>
       )}
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay>
+          <ModalContent>
+            <ModalHeader>ユーザー詳細</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              <Stack>
+                <FormControl>名前</FormControl>
+                <Input value="はち" isReadOnly />
+              </Stack>
+            </ModalBody>
+          </ModalContent>
+        </ModalOverlay>
+      </Modal>
     </>
   );
 });
