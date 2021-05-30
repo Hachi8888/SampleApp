4202ae1a3c4798b6c2190669f027c41aa348be31
hachi8888 <53389277+Hachi8888@users.noreply.github.com>
2021-05-30 23:26:01 +0900

    [add] show each user at Modal

--- src/components/organisms/user/UserCard.tsx
+++ src/components/organisms/user/UserCard.tsx
@@ -2,14 +2,15 @@ import { Box, Image, Stack, Text } from "@chakra-ui/react";
 import { memo, VFC } from "react";
 
 type Props = {
+  id: number;
   imageUrl: string;
   userName: string;
   fullName: string;
-  onClickUser: () => void;
+  onClickUser: (id: number) => void;
 };
 
 export const UserCard: VFC<Props> = memo((props) => {
-  const { imageUrl, userName, fullName, onClickUser } = props;
+  const { imageUrl, userName, fullName, onClickUser, id } = props;
   return (
     <Box
       w="260px"
@@ -19,7 +20,7 @@ export const UserCard: VFC<Props> = memo((props) => {
       shadow="md"
       p={4}
       _hover={{ cursor: "pointer", opacity: 0.8 }}
-      onClick={onClickUser}
+      onClick={() => onClickUser(id)}
     >
       <Stack textAlign="center">
         <Image
