72298e93e7b716feef38b785966d38ae8768f354
hachi8888 <53389277+Hachi8888@users.noreply.github.com>
2021-06-06 21:19:38 +0900

    [add] update function

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -18,7 +18,6 @@ export const UserManagement: VFC = memo(() => {
   const { getUsers, users, loading } = useAllUsers();
   const { onSelectUser, selectedUser } = useSelectUser();
   const { loginUser } = useLoginUser();
-  console.log(loginUser);
 
   useEffect(() => getUsers(), []);
   const onClickUser = useCallback(
@@ -49,7 +48,12 @@ export const UserManagement: VFC = memo(() => {
           ))}
         </Wrap>
       )}
-      <UserDetailModal user={selectedUser} isOpen={isOpen} onClose={onClose} />
+      <UserDetailModal
+        user={selectedUser}
+        isOpen={isOpen}
+        isAdmin={loginUser?.isAdmin}
+        onClose={onClose}
+      />
     </>
   );
 });
